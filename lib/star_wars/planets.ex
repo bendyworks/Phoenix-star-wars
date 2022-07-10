4a9534f7cd5d19eccf7ea4eb709a5f489b98ca6f
Roger Roelofs <roger.roelofs@gmail.com>
2022-07-10 14:02:07 -0400

    extract columns and remove converter

--- lib/star_wars/planets.ex
+++ lib/star_wars/planets.ex
@@ -5,54 +5,6 @@ defmodule StarWars.Planets do
 
   alias StarWars.Repo
   alias StarWars.Planets.Planet
-  alias HTTPoison
-
-  @base "https://swapi.dev/api/"
-
-  @doc """
-  Returns the list of planets.
-
-  ## Examples
-
-      iex> list_planets()
-      [%Planet{}, ...]
-
-  """
-  def list_planets_json do
-    fetch_json("planets")
-  end
-
-  @doc """
-  Gets a single planet.
-
-  Raises `Ecto.NoResultsError` if the Planet does not exist.
-
-  ## Examples
-
-      iex> get_planet(123)
-      %{}
-
-      iex> get_planet(456)
-      %{}
-
-  """
-  def get_planet_json!(id), do: fetch_json("planets/#{id}")
-
-  defp fetch_json(path) do
-    url = "#{@base}#{path}"
-    # IO.inspect(url)
-
-    case HTTPoison.get(url) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)["results"]}
-
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, :not_found}
-
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
-  end
 
   @doc """
   Returns the list of planets.
