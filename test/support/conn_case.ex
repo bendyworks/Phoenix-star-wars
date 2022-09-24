31630ed6b9f088548688875daf97398a8795290e
Roger Roelofs <roger.roelofs@gmail.com>
2022-09-24 16:41:56 -0400

    fix test and update config

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -31,8 +31,8 @@ defmodule StarWarsWeb.ConnCase do
     end
   end
 
-  setup _tags do
-    # StarWars.DataCase.setup_sandbox(tags)
+  setup tags do
+    StarWars.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
